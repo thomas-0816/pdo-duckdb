406b8d90a1ba7a02e35de6505e582208d6b8fd67
Thomas Bley <mails@thomasbley.de>
2026-06-20 01:39:11 +0200

    added decimal test

--- test.php
+++ test.php
@@ -4,11 +4,15 @@ error_reporting(E_ALL);
 ini_set('display_errors', 1);
 
 $db = new PDO('duckdb::memory:');
-$db->exec("CREATE TABLE t (i INTEGER, b BIGINT, d DECIMAL(10,2), v VARCHAR)");
+$db->exec("CREATE TABLE t (i INTEGER, b BIGINT, d DECIMAL(10, 2), v VARCHAR)");
 $stmt = $db->prepare("INSERT INTO t VALUES (?, ?, ?, ?)");
-$stmt->execute([1, 9223372036854775807, 3.14151221312, 'hello']);
-$stmt = $db->query("SELECT * FROM t");
+$stmt->execute([1, 9223372036854775807, 3.141511313212312312, 'hello']);
+$stmt = $db->query("SELECT * FROM t", PDO::FETCH_ASSOC);
 while ($row = $stmt->fetch()) { print_r($row); }
+
+if (file_exists('/tmp/pdo_duckdb_test.db')) {
+    unlink('/tmp/pdo_duckdb_test.db');
+}
 
 $db = new PDO('duckdb:/tmp/pdo_duckdb_test.db');
 $db->exec("CREATE TABLE t (i INTEGER, v VARCHAR)");
