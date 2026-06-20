5d59e4f2ca65c732616016f8efc1f3f2be483a38
Thomas Bley <mails@thomasbley.de>
2026-06-20 08:56:16 +0200

    cleanup

--- duckdb_stubs.cpp
+++ duckdb_stubs.cpp
@@ -2,7 +2,8 @@ namespace duckdb {
 
 class DuckDB;
 
-} // namespace duckdb
-
-extern "C" void _ZTVN10__cxxabiv120__si_class_type_infoE(duckdb::DuckDB &) {
+namespace ExtensionHelper {
+void LoadAllExtensions(DuckDB &) {}
 }
+
+} // namespace duckdb
