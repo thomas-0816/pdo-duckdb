e8d7601636c775c704f9d1d9187b95b0b200749f
Thomas Bley <mails@thomasbley.de>
2026-06-21 03:49:48 +0200

    dropped wrong license headers, license is MIT

--- php_pdo_duckdb_int.h
+++ php_pdo_duckdb_int.h
@@ -1,21 +1,3 @@
-/*
-  +----------------------------------------------------------------------+
-  | PDO_DuckDB - DuckDB driver for PDO                                    |
-  +----------------------------------------------------------------------+
-  | Copyright (c) 1997-2025 The PHP Group                                |
-  +----------------------------------------------------------------------+
-  | This source file is subject to version 3.01 of the PHP license,      |
-  | that is bundled with this package in the file LICENSE, and is        |
-  | available through the world-wide-web at the following url:           |
-  | http://www.php.net/license/3_01.txt                                  |
-  | If you did not receive a copy of the PHP license and are unable to   |
-  | obtain it through the world-wide-web, please send a note to          |
-  | license@php.net so we can mail you a copy immediately.               |
-  +----------------------------------------------------------------------+
-  | Author: PDO_DuckDB Contributors                                       |
-  +----------------------------------------------------------------------+
- */
-
 #ifndef PHP_PDO_DUCKDB_INT_H
 #define PHP_PDO_DUCKDB_INT_H
 
