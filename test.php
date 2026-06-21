d9b618989c31db910f966d76787d5542620c38af
Thomas Bley <mails@thomasbley.de>
2026-06-21 17:31:53 +0200

    use : for named bindings in php

--- test.php
+++ test.php
@@ -586,14 +586,14 @@ print_r($db->query('SELECT * FROM t')->fetchAll(PDO::FETCH_ASSOC));
 
 $db = new PDO('duckdb::memory:');
 $db->exec("CREATE TABLE t (n INTEGER NULL, i INTEGER NULL, b BIGINT NULL, d DECIMAL(10, 2) NULL, v VARCHAR NULL)");
-$statement = $db->prepare('INSERT INTO t VALUES ($aa, $bb, $cc, $dd, $ee)');
+$statement = $db->prepare('INSERT INTO t VALUES (:aa, :bb, :cc, :dd, :ee)');
 $statement->execute(['bb' => 2, 'aa' => 1, 'cc' => 9223372036854775807, 'ee' => 'hello', 'dd' => 3.141511313212312312]);
 $statement->execute(['bb' => null, 'aa' => null, 'cc' => null, 'ee' => null, 'dd' => null]);
 print_r($db->query('SELECT * FROM t')->fetchAll(PDO::FETCH_ASSOC));
 
 $db = new PDO('duckdb::memory:');
 $db->exec("CREATE TABLE t (n INTEGER NULL, i INTEGER NULL, b BIGINT NULL, d DECIMAL(10, 2) NULL, v VARCHAR NULL)");
-$statement = $db->prepare('INSERT INTO t VALUES ($aa, $bb, $cc, $dd, $ee)');
+$statement = $db->prepare('INSERT INTO t VALUES (:aa, :bb, :cc, :dd, :ee)');
 try {
     $statement->execute(['bb' => 2, 'aa' => 1]);
 } catch (Exception $e) {
@@ -602,7 +602,7 @@ try {
 
 $db = new PDO('duckdb::memory:');
 $db->exec("CREATE TABLE t (n INTEGER NULL, i INTEGER NULL, b BIGINT NULL, d DECIMAL(10, 2) NULL, v VARCHAR NULL)");
-$statement = $db->prepare('INSERT INTO t VALUES ($aa, $bb, $cc, $dd, $ee)');
+$statement = $db->prepare('INSERT INTO t VALUES (:aa, :bb, :cc, :dd, :ee)');
 $statement->bindValue('aa', null, PDO::PARAM_NULL);
 $statement->bindValue('bb', 201, PDO::PARAM_INT);
 $statement->bindValue('cc', 300, PDO::PARAM_INT);
@@ -616,7 +616,7 @@ $statement->bindValue(3, 300);
 $statement->bindValue(4, 42.21);
 $statement->bindValue(5, 'test');
 $statement->execute();
-$statement = $db->prepare('INSERT INTO t VALUES ($aa, $bb, $cc, $dd, $ee)');
+$statement = $db->prepare('INSERT INTO t VALUES (:aa, :bb, :cc, :dd, :ee)');
 $statement->bindValue('aa', null);
 $statement->bindValue('bb', 203);
 $statement->bindValue('cc', 300);
@@ -625,7 +625,7 @@ $statement->bindValue('ee', 'test');
 $statement->execute();
 print_r($db->query('SELECT * FROM t')->fetchAll(PDO::FETCH_ASSOC));
 
-$statement = $db->prepare('INSERT INTO t VALUES ($aa, $bb, $cc, $dd, $ee)');
+$statement = $db->prepare('INSERT INTO t VALUES (:aa, :bb, :cc, :dd, :ee)');
 $statement->bindValue('aa', null);
 $statement->bindValue('bb', 203);
 try {
@@ -699,7 +699,7 @@ var_dump($statement->fetchAll(PDO::FETCH_COLUMN));
 $db->exec("CREATE TABLE t (v VARCHAR)");
 $statement = $db->prepare("INSERT INTO t VALUES (?)");
 $statement->execute(["he\0llo"]);
-$statement = $db->prepare('INSERT INTO t VALUES ($aa)');
+$statement = $db->prepare('INSERT INTO t VALUES (:aa)');
 $statement->bindValue('aa', "he\0llo", PDO::PARAM_STR);
 $statement->execute();
 $statement = $db->prepare("INSERT INTO t VALUES (?)");
